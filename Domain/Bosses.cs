0b9efe3f30b17b9885f1e27b3f802aaff2841eb6
Dinislam Gilyazov <den.gilyazov@yandex.ru>
2023-01-22 20:33:29 +0500

    init:test

--- Domain/Bosses.cs
+++ Domain/Bosses.cs
@@ -10,6 +10,8 @@ namespace Domain
         public int Id { get; set; }
         public string? Name { get; set; }
 
+        public string? test { get; set; }
+
         public string? Description { get; set; }
 
 
